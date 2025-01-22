6ef8747cf03e2fe0c4a8d74621433e8675e4e696
JohnGiampaolo <g.giampaolo03@gmail.com>
2025-01-22 17:18:20 +0800

    Removed non-definitive sentences from extraction

--- WordNet Extraction/sentencer.py
+++ WordNet Extraction/sentencer.py
@@ -12,20 +12,21 @@ def extractor(file_path):
             definitions = []
             #Extract definitions from each line
             for line in file:
-                matches = reg_parentheses(line)
-                for match in matches:
-                    regex = re.compile('[,\.!?;""<>\(\)`]')
-                    match = regex.sub('', match)
-                    match_dict = {}
-                    match_dict["definition"] = match
-                    definitions.append(match_dict)
+                if line[0] != "(":
+                    matches = reg_parentheses(line)
+                    for match in matches:
+                        regex = re.compile('[,\.!?;""<>\(\)`]')
+                        match = regex.sub('', match)
+                        if ' ' in match:
+                            match_dict = {}
+                            match_dict["definition"] = match
+                            definitions.append(match_dict)
                   
             # Having grouped all definitions into a list of dictionaries
             # Write each one to CSV file
             with open("test.csv", "w", newline='') as csvfile:
                 fieldnames=["definition"]
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
                 writer.writerows(definitions)
                 
     except Exception as e:
