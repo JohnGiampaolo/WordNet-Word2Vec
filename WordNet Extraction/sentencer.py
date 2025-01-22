e606d390982993779a9d21718a6116bf3fb4a7cc
JohnGiampaolo <g.giampaolo03@gmail.com>
2025-01-22 17:41:50 +0800

    All definitions write to one csv file

--- WordNet Extraction/sentencer.py
+++ WordNet Extraction/sentencer.py
@@ -1,39 +1,44 @@
 import regex as re
 import csv 
+import os
+
 def reg_parentheses(line):
     pattern = r'(\([^()]*\([^()]*\)[^()]*\)|\([^()]*\))'
     matches = re.findall(pattern, line)
     return matches
 
-def extractor(file_path):
+def extractor(dir_path):
     try:
-        with open(file_path, "r") as file:
-            #Data will contain a list of dictionary mappings for ease of CSV writing
-            definitions = []
-            #Extract definitions from each line
-            for line in file:
-                if line[0] != "(":
-                    matches = reg_parentheses(line)
-                    for match in matches:
-                        regex = re.compile('[,\.!?;""<>\(\)`]')
-                        match = regex.sub('', match)
-                        if ' ' in match:
-                            match_dict = {}
-                            match_dict["definition"] = match
-                            definitions.append(match_dict)
-                  
-            # Having grouped all definitions into a list of dictionaries
-            # Write each one to CSV file
-            with open("test.csv", "w", newline='') as csvfile:
-                fieldnames=["definition"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerows(definitions)
+        #Data will contain a list of dictionary mappings for ease of CSV writing
+        definitions = []
+        for filename in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, filename)
+            if os.path.isfile(file_path):
+                with open(file_path, "r") as file:
+                    #Extract definitions from each line
+                    for line in file:
+                        if line[0] != "(":
+                            matches = reg_parentheses(line)
+                            for match in matches:
+                                regex = re.compile('[,\.!?;""<>\(\)`]')
+                                match = regex.sub('', match)
+                                if ' ' in match:
+                                    match_dict = {}
+                                    match_dict["definition"] = match
+                                    definitions.append(match_dict)
+                        
+        # Having grouped all definitions into a list of dictionaries
+        # Write each one to CSV file
+        with open("test.csv", "w", newline='') as csvfile:
+            fieldnames=["definition"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerows(definitions)
                 
     except Exception as e:
-        print(f"Error reading {file_path}: {e}")
+        print(f"Error reading {dir_path}: {e}")
         
 def main():
-    user_input = input("File path of dict.db: ")
+    user_input = input("Directory path: ")
     extractor(user_input)
     
 main()
